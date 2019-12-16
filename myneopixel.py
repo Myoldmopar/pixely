5e439ee378ee779ada6e15144cb1ce77ef53bc2e
Edwin Lee <leeed2001@gmail.com>
2019-12-16 21:58:36 +0000

    Initial commit

---
+++ myneopixel.py
@@ -0,0 +1,41 @@
+from rpi_ws281x import PixelStrip, Color
+import time
+
+LED_COUNT = 144        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 0 # initially   # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+class Colors:
+    GREEN = Color(0, 255, 0)
+    BLUE = Color(0, 0, 255)
+    RED = Color(255, 0, 0)
+
+
+def fade_in_time_and_values(num_steps, total_time, start_bright, end_bright):
+    total_distance = end_bright - start_bright
+    increment = total_distance / num_steps
+    step_time = total_time / 50
+    bright_values = [start_bright]
+    this_bright = start_bright
+    while this_bright <= end_bright:
+        this_bright = this_bright + increment
+        bright_values.append(this_bright)
+    return step_time, bright_values
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+for i in range(strip.numPixels()):
+    strip.setPixelColor(i, Colors.GREEN)
+
+delta_t, brights = fade_in_time_and_values(100, 2, 0, 255)
+for brightness in brights:
+    strip.setBrightness(int(brightness))
+    time.sleep(delta_t)
+    strip.show()
+
