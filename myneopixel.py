a3d9e14b214fd716243629b8cb4235b0faea5004
Edwin Lee <leeed2001@gmail.com>
2019-12-16 18:16:53 -0600

    More!!!

--- myneopixel.py
+++ myneopixel.py
@@ -53,7 +53,7 @@ c = 2 * math.pi / DIVISIONS
 offsets = [
     -0 * c, -1 * c , -2 * c, -3 * c, -4 * c, -5 * c,
     -6 * c, -7 * c , -8 * c, -9 * c, -10 * c, -11 * c,
-] * 3
+] * 10
 
 for offset in offsets:
     for pixel in range(LED_COUNT):
